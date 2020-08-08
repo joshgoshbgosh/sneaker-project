1fd5d612ab2f371b9e72921568a05365584a7f7d
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2020-08-08 08:48:48 -0400

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("hello...its me");
+var magicNumber = 1;
+var sum = magicNumber * magicNumber
+console.log(sum);
+// person object
+var person = {
+  name: "Jenny", // name is a property
+  age: 45,
+  favoriteColor: "blue"
+}
+console.log(blue);
