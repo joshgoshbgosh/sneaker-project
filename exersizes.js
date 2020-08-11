86859d44d429b30cfa3d5d5041f23431421a3cfb
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2020-08-10 23:17:21 -0400

    exersize 2

--- exersizes.js
+++ exersizes.js
@@ -15,10 +15,18 @@ console.log("exersizeone",twoPlusTwo) // 4
 // Exercise 2
 // Write a function called yell that logs out an uppercase version of a string.
 
+function yell(number) {
+  return number * number;
+  console.log(yell.toUpperCase)
+}
+
+
 
 
 // Write a function called yell10 that uses your yell function to log out an
 // uppercase version of a string 10 times.
+
+
 
 // Exercise 3
 // Write a function called longest that returns the longest of two input strings
